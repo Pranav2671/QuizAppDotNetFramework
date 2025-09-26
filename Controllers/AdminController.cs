e1dd1006a8adef9d5e6ee466593d714a4bc97428
Pranav2671 <pranavbabu26.7@gmail.com>
2025-09-26 16:47:13 +0530

    Added Update Quiz View named UpdateQuiz.cshtml

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -7,77 +7,27 @@ namespace QuizAppDotNetFramework.Controllers
 {
     public class AdminController : Controller
     {
-        private QuizRepository quizRepo = new QuizRepository();
+        private QuizRepository _quizRepository = new QuizRepository();
 
-        // GET: Display all quizzes
-        public ActionResult Index()
+        // GET: load quiz to edit
+        public ActionResult UpdateQuiz(Guid id)
         {
-            var quizzes = quizRepo.GetAllQuizzes();
-            return View(quizzes);
-        }
-
-        // GET: Load Update Quiz form
-        [HttpGet]
-        public ActionResult UpdateQuiz(Guid quizId)
-        {
-            var quiz = quizRepo.GetQuizById(quizId);
+            var quiz = _quizRepository.GetQuizById(id);
             return View(quiz);
         }
 
-        // POST: Update Quiz
+        // POST: save edited quiz
         [HttpPost]
-        public ActionResult UpdateQuiz(QuizModel quiz)
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateQuiz(QuizModel model)
         {
-            try
+            if (ModelState.IsValid)
             {
-                quizRepo.UpdateQuiz(quiz);
+                _quizRepository.UpdateQuiz(model);
                 TempData["Success"] = "Quiz updated successfully!";
-                return RedirectToAction("Index");
+                return RedirectToAction("Index"); // Quiz list page
             }
-            catch (Exception ex)
-            {
-                ViewBag.Error = "Update failed: " + ex.Message;
-                return View(quiz);
-            }
-        }
-
-        // GET: Add new quiz
-        [HttpGet]
-        public ActionResult AddQuiz()
-        {
-            return View(new QuizModel());
-        }
-
-        // POST: Add new quiz
-        [HttpPost]
-        public ActionResult AddQuiz(QuizModel quiz)
-        {
-            try
-            {
-                quizRepo.AddQuiz(quiz);
-                TempData["Success"] = "Quiz added successfully!";
-                return RedirectToAction("Index");
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Error = "Add failed: " + ex.Message;
-                return View(quiz);
-            }
-        }
-
-        // Delete Quiz
-        public ActionResult DeleteQuiz(Guid quizId)
-        {
-            try
-            {
-                quizRepo.DeleteQuiz(quizId);
-                TempData["Success"] = "Quiz deleted successfully!";
-            }
-            catch (Exception ex)
-            {
-                TempData["Error"] = "Delete failed: " + ex.Message;
-            }
-            return RedirectToAction("Index");
+            return View(model);
         }
     }
 }
--- Repository/QuizRepository.cs
+++ Repository/QuizRepository.cs
@@ -12,151 +12,79 @@ namespace QuizAppDotNetFramework.Repository
     {
         private string connectionString = ConfigurationManager.ConnectionStrings["QuizDbConnection"].ConnectionString;
 
-        // Get all quizzes
-        public List<QuizModel> GetAllQuizzes()
-        {
-            List<QuizModel> quizzes = new List<QuizModel>();
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Quizzes", con))
-                {
-                    con.Open();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            quizzes.Add(new QuizModel
-                            {
-                                QuizId = (Guid)reader["QuizId"],
-                                Title = reader["Title"].ToString(),
-                                CreatedBy = (Guid)reader["CreatedBy"],
-                                CreatedDate = (DateTime)reader["CreatedDate"],
-                                Questions = new List<QuestionModel>() // initialize empty
-                            });
-                        }
-                    }
-                }
-            }
-
-            return quizzes;
-        }
-
-        // Get a single quiz with questions and options
+        // Get quiz by ID
         public QuizModel GetQuizById(Guid quizId)
         {
-            QuizModel quiz = new QuizModel();
-
+            QuizModel quiz = null;
             using (SqlConnection con = new SqlConnection(connectionString))
             {
+                SqlCommand cmd = new SqlCommand("sp_GetQuizById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@QuizId", quizId);
+
                 con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
 
-                // Quiz details
-                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Quizzes WHERE QuizId=@QuizId", con))
+                quiz = new QuizModel();
+                quiz.Questions = new List<QuestionModel>();
+
+                while (reader.Read())
                 {
-                    cmd.Parameters.AddWithValue("@QuizId", quizId);
-                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    if (quiz.QuizId == Guid.Empty)
                     {
-                        if (reader.Read())
+                        quiz.QuizId = reader.GetGuid(reader.GetOrdinal("QuizId"));
+                        quiz.Title = reader.GetString(reader.GetOrdinal("Title"));
+                        quiz.CreatedBy = reader.GetGuid(reader.GetOrdinal("CreatedBy"));
+                        quiz.CreatedDate = reader.GetDateTime(reader.GetOrdinal("CreatedDate"));
+                    }
+
+                    // Read questions
+                    Guid questionId = reader.GetGuid(reader.GetOrdinal("QuestionId"));
+                    QuestionModel question = quiz.Questions.Find(q => q.QuestionId == questionId);
+                    if (question == null)
+                    {
+                        question = new QuestionModel
                         {
-                            quiz.QuizId = (Guid)reader["QuizId"];
-                            quiz.Title = reader["Title"].ToString();
-                            quiz.CreatedBy = (Guid)reader["CreatedBy"];
-                            quiz.CreatedDate = (DateTime)reader["CreatedDate"];
-                        }
+                            QuestionId = questionId,
+                            QuizId = quiz.QuizId,
+                            QuestionText = reader.GetString(reader.GetOrdinal("QuestionText")),
+                            Options = new List<OptionModel>()
+                        };
+                        quiz.Questions.Add(question);
+                    }
+
+                    // Read options
+                    if (!reader.IsDBNull(reader.GetOrdinal("OptionId")))
+                    {
+                        OptionModel option = new OptionModel
+                        {
+                            OptionId = reader.GetGuid(reader.GetOrdinal("OptionId")),
+                            QuestionId = questionId,
+                            OptionText = reader.GetString(reader.GetOrdinal("OptionText")),
+                            IsCorrect = reader.GetBoolean(reader.GetOrdinal("IsCorrect"))
+                        };
+                        question.Options.Add(option);
                     }
                 }
-
-                // Questions
-                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Questions WHERE QuizId=@QuizId", con))
-                {
-                    cmd.Parameters.AddWithValue("@QuizId", quizId);
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        quiz.Questions = new List<QuestionModel>();
-                        while (reader.Read())
-                        {
-                            quiz.Questions.Add(new QuestionModel
-                            {
-                                QuestionId = (Guid)reader["QuestionId"],
-                                QuizId = (Guid)reader["QuizId"],
-                                QuestionText = reader["QuestionText"].ToString(),
-                                Options = new List<OptionModel>() // initialize empty
-                            });
-                        }
-                    }
-                }
-
-                // Options for each question
-                foreach (var question in quiz.Questions)
-                {
-                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM Options WHERE QuestionId=@QuestionId", con))
-                    {
-                        cmd.Parameters.AddWithValue("@QuestionId", question.QuestionId);
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                question.Options.Add(new OptionModel
-                                {
-                                    OptionId = (Guid)reader["OptionId"],
-                                    QuestionId = (Guid)reader["QuestionId"],
-                                    OptionText = reader["OptionText"].ToString(),
-                                    IsCorrect = (bool)reader["IsCorrect"]
-                                });
-                            }
-                        }
-                    }
-                }
+                con.Close();
             }
-
             return quiz;
         }
 
-        // Add quiz using JSON stored procedure
-        public void AddQuiz(QuizModel quiz)
-        {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand("sp_AddQuiz", con))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    string jsonData = JsonConvert.SerializeObject(quiz);
-                    cmd.Parameters.AddWithValue("@JsonData", jsonData);
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        // Update quiz using JSON stored procedure
+        // Update quiz using JSON
         public void UpdateQuiz(QuizModel quiz)
         {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand("sp_UpdateQuiz", con))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    string jsonData = JsonConvert.SerializeObject(quiz);
-                    cmd.Parameters.AddWithValue("@JsonData", jsonData);
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
+            string jsonData = JsonConvert.SerializeObject(quiz);
 
-        // Delete quiz
-        public void DeleteQuiz(Guid quizId)
-        {
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                using (SqlCommand cmd = new SqlCommand("sp_DeleteQuiz", con))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@QuizId", quizId);
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                }
+                SqlCommand cmd = new SqlCommand("sp_UpdateQuiz", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@JsonData", jsonData);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
             }
         }
     }
